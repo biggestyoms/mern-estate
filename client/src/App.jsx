7cdf50e36fe421f94a05fd5cb83a472bef3fc7ea
biggestyoms <oladimejiyomica@outlook.com>
2024-05-10 09:13:44 -0400

    make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
+
 
 
 export default function App() {
@@ -15,9 +17,10 @@ export default function App() {
     <Route path='/' element={<Home/>}/>
     <Route path='/signin' element={<SignIn/>}/>
     <Route path='/signup' element={<SignUp/>}/>
-    <Route path='/about' element={<About/>}/>
-    <Route path='/profile' element={<Profile/>}/>
-    
+    <Route path='/about' element={<About/>}/> 
+    <Route path='/' element= {<PrivateRoute/>} >
+   <Route path='/profile' element={<Profile/>}/>
+   </Route>
    </Routes>
    </BrowserRouter>
   )
